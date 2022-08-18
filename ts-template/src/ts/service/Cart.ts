4a19d2e8da884a369a5aea68d43be12370fbf9a4
AlenaKarpolenko <alenac190590@mail.ru>
2022-08-18 22:18:54 +0700

    added

--- ts-template/src/ts/service/Cart.ts
+++ ts-template/src/ts/service/Cart.ts
@@ -8,6 +8,26 @@ export default class Cart {
     }
 
     get items(): Buyable[] {
-        return [...this._items]; 
+        return [...this._items];
+    }
+
+    calculateTotalPrice(): number {
+        return this._items.reduce((sum, current) => sum + current.price, 0)
+    }
+
+    calculatePriceWithDiscount(discount: number): number {
+        if (discount < 0 || discount > 100) {
+            throw new Error ('Скидка может быть от 0 до 100')
+        }
+        return this.calculateTotalPrice() * (100 - discount) / 100;
+    }
+
+    remove(id: number): boolean {
+        const index = this._items.findIndex((item) => item.id === id);
+        if (index === -1 ) {
+            return false;
+        }
+        this._items.splice(index, 1);
+        return true;
     }
 }
